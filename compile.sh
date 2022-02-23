43204d415f6ab513fcf2bc841325a9ecd0c8e3a9
ediah <60201514+ediah@users.noreply.github.com>
2022-02-24 00:49:35 +0300

    Исправил

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
 files=($(ls src))
 for file in "${files[@]}"; do
-    echo ./src/$file
-    #latexmk -pdf -outdir=./pages ./src/$file
-    #latexmk -c -outdir=./pages ./src/$file
+    latexmk -pdf -outdir=./pages ./src/$file
+    latexmk -c -outdir=./pages ./src/$file
 done
