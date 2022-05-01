f12efee3df2d143312f1bc68b5a6df31e0e66bdd
ediah <60201514+ediah@users.noreply.github.com>
2022-05-01 19:35:09 +0300

    Новые домашние работы по ЭКА и МОК, разбил большие файлы на меньшие части

--- compile.sh
+++ compile.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
 cd src
 files=($(ls *.tex))
 for file in "${files[@]}"; do
-    latexmk -pdf -outdir=../pages ./$file
+    if [[ "$1" == "draft" ]]; then
+        latexmk -pdf -pdflatex='pdflatex -draftmode %O %S && touch %D' -print=pdf -e '$lpr_pdf=q|pdflatex -interaction=batchmode -synctex=1 %R|' -outdir=../pages ./$file
+    else
+        latexmk -pdf -outdir=../pages ./$file
+    fi
     latexmk -c -outdir=../pages ./$file
 done
